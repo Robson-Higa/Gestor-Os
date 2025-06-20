553b721675c52803c4a779fc23eae1bfcca771d7
Robson-Higa <higarobson@gmail.com>
2025-06-20 12:29:23 -0400

    commit inicial

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,33 @@
+// src/contexts/AuthContext.tsx
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../services/firebaseConfig";
+
+interface AuthContextProps {
+  user: User | null;
+  loading: boolean;
+}
+
+const AuthContext = createContext<AuthContextProps>({ user: null, loading: true });
+
+export const AuthProvider = ({ children }: any) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
